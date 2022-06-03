48aa521dcfe3d712dbf0cd8eafe208c2e1400711
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-06-03 10:17:21 +0200

    Dynamically display maze with walls, entrance, exit

--- index.js
+++ index.js
@@ -6,16 +6,37 @@ root.style.setProperty("--maze-size", size);
 
 document.getElementById("maze").textContent = JSON.stringify(labyrinths[size]["ex-0"], undefined, 2);
 
-
-
 const outline = document.getElementById('outline');
 
 for(let i in labyrinths[size]["ex-0"]) {
 
-    let div = document.createElement('div');
+    let createdCell = labyrinths[size]["ex-0"][i]
 
-    div.classList.add('cell');
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    outline.append(cell);
 
-    outline.append(div);
+    if(createdCell.entrance == true) {
+        cell.classList.add('entrance');
+    }
+    if(createdCell.exit) {
+        cell.classList.add('exit');
+    }
+
+    if(createdCell.walls[0]) {
+        cell.classList.add('top');
+    }
+
+    if(createdCell.walls[1]) {
+        cell.classList.add('right');
+    }
+
+    if(createdCell.walls[2]) {
+        cell.classList.add('bottom');
+    }
+
+    if(createdCell.walls[3]) {
+        cell.classList.add('left');
+    }
 }
 
