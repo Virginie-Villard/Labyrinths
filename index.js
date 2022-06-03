3dd8ea37313e2cae5b6094933364b63eb09859fc
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-06-03 16:27:16 +0200

    Add pseudoCode

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
-const size = 5;
+// document.getElementsByTagName('h1').style.textAlign = "center";
+
+const size = 10;
 
 const root = document.documentElement;
 root.style.setProperty("--maze-size", size);
 
-document.getElementById("maze").textContent = JSON.stringify(labyrinths[size]["ex-0"], undefined, 2);
+document.getElementById("maze").textContent = JSON.stringify(labyrinths[size]["ex-2"], undefined, 2);
+document.getElementById("maze").style.display = "none";
 
 const outline = document.getElementById('outline');
 
-for(let i in labyrinths[size]["ex-0"]) {
+for(let i in labyrinths[size]["ex-2"]) {
 
-    let createdCell = labyrinths[size]["ex-0"][i]
+    let createdCell = labyrinths[size]["ex-2"][i]
 
     let cell = document.createElement('div');
     cell.classList.add('cell');
@@ -39,4 +42,6 @@ for(let i in labyrinths[size]["ex-0"]) {
         cell.classList.add('left');
     }
 }
+
+
 
---
+++ styles/pseudoCode.js
@@ -0,0 +1,109 @@
+/*
+//start
+let position; // = findEntrance();
+
+
+if(cell.walls) {
+    // Ne peut jamais traverser
+}
+// ____________________________________
+
+if(cell.walls.length === 3) {
+    // avance sur la case sans mur
+}
+// ____________________________________
+
+if(cell.walls.length === 2) {
+    // choose(the only other way)
+}
+// ____________________________________
+
+if(cell.walls.length > 2) {
+    // choose(way)
+}
+// ____________________________________
+
+
+if(cell.walls.length === 2) {
+    // avance sur la case de laquelle tu ne viens pas
+}
+
+if(position === cell.exit) {
+    // "Win !"
+}
+
+
+// _Functions______________________________________________________________
+
+function findEntrance() {
+    for(i in cells) {
+        return cell.entrance = true;
+    }
+}
+
+// ____________________________________
+function myNeighbours(position) {
+    if(cell.walls[0]) {
+        goTop = false
+    }
+
+    if(cell.walls[1]) {
+        goRight = false
+    }
+
+    if(cell.walls[2]) {
+        goBottom = false
+    }
+
+    if(cell.walls[3]) {
+        goLeft = false
+    }
+}
+
+// ____________________________________
+function goRight(position) {
+    position = div[i + 1];
+}
+function goDown(position) {
+    position = div[i + size];
+}
+function goTop(position) {
+    position = div[i - size];
+}
+function goLeft(position) {
+    position = div[i - 1];
+}
+// ____________________________________
+
+
+function choose(way) {
+
+    let n; // n = nombre de possibilités; (entre 1 et 4)
+
+    // if(n > 2) {
+    //     // Math.floor(n) * Math.random();
+    // }
+
+    // choisis gauche.
+
+}
+
+function isVisited(cell) {
+    let cell.visited = 0;
+
+    if(cell.visited) {
+        visited ++;
+    }
+
+    return visited;
+}
+
+// ________________________________________________________________________
+
+
+
+
+
+
+
+*/
