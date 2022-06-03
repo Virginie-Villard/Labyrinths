886c364038e8a180794cb3b533aa7470ca656e32
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-06-03 09:31:53 +0200

    Dynamically change the size of the maze in CSS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
-document.getElementById("maze").textContent = JSON.stringify(labyrinths["3"]["ex-0"], undefined, 2);
+const size = 5;
+
+const root = document.documentElement;
+root.style.setProperty("--maze-size", size);
+
+document.getElementById("maze").textContent = JSON.stringify(labyrinths[size]["ex-0"], undefined, 2);
 
 
 
 const outline = document.getElementById('outline');
 
-for(let i in labyrinths["3"]["ex-0"]) {
+for(let i in labyrinths[size]["ex-0"]) {
 
     let div = document.createElement('div');
 
